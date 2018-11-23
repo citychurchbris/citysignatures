65c398c4e708d5d339be897454e1a16030b62282
Matthew Sital-Singh <mattss@netsight.co.uk>
2018-11-23 16:04:11 +0000

    Update with themes and logos

--- api.py
+++ api.py
@@ -2,15 +2,45 @@ import responder
 
 api = responder.API()
 
+themes = {
+    'Purple': {
+        'hex1': '#5A2D72',
+        'hex2': '#000000',
+        'logo': 'https://www.citychurch.org.uk/ext_assets/signatures/cc_siglogo_purple.png',
+    },
+    'Blue': {
+        'hex1': '#2158A8',
+        'hex2': '#000000',
+        'logo': 'https://www.citychurch.org.uk/ext_assets/signatures/cc_siglogo_blue.png',
+    },
+    'Teal': {
+        'hex1': '#4BBEAD',
+        'hex2': '#000000',
+        'logo': 'https://www.citychurch.org.uk/ext_assets/signatures/cc_siglogo_teal.png',
+    },
+    'Red': {
+        'hex1': '#E5264A',
+        'hex2': '#000000',
+        'logo': 'https://www.citychurch.org.uk/ext_assets/signatures/cc_siglogo_red.png',
+    },
+    'Black': {
+        'hex1': '#000000',
+        'hex2': '#555555',
+        'logo': 'https://www.citychurch.org.uk/ext_assets/signatures/cc_siglogo_darkgrey.png',
+    },
+}
+
 
 @api.route("/")
 def index(req, resp):
-    resp.content = api.template('index.html')
+    resp.content = api.template('index.html', themes=themes)
 
 
 @api.route("/generate")
 def generate(req, resp):
-    resp.content = api.template('signature.html', params=req.params)
+    theme = themes.get(req.params['theme'])
+    resp.content = api.template(
+        'signature.html', params=req.params, theme=theme)
 
 
 if __name__ == "__main__":
